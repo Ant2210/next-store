b144dd5924e78fd4a60c83a70f005b19b9a6e3cd
Ant <ant.griffiths22@gmail.com>
2024-09-23 12:20:47 +0100

    Orders and stripe

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,11 +1,35 @@
-import { Button } from '@/components/ui/button';
+'use client';
+
+import { useState } from 'react';
+import SelectProductAmount from './SelectProductAmount';
+import { Mode } from './SelectProductAmount';
+import FormContainer from '../form/FormContainer';
+import { SubmitButton } from '../form/Buttons';
+import { addToCartAction } from '@/utils/actions';
+import { useAuth } from '@clerk/nextjs';
+import { ProductSignInButton } from '../form/Buttons';
 
 const AddToCart = ({ productId }: { productId: string }) => {
-	console.log(productId);
+	const [amount, setAmount] = useState(1);
+	const { userId } = useAuth();
+
 	return (
-		<Button className="mt-8" size="lg">
-			Add To Cart
-		</Button>
+		<div className="mt-4">
+			<SelectProductAmount
+				mode={Mode.SingleProduct}
+				amount={amount}
+				setAmount={setAmount}
+			/>
+			{userId ? (
+				<FormContainer action={addToCartAction}>
+					<input type="hidden" name="productId" value={productId} />
+					<input type="hidden" name="amount" value={amount} />
+					<SubmitButton text="Add to cart" className="mt-8"></SubmitButton>
+				</FormContainer>
+			) : (
+				<ProductSignInButton />
+			)}
+		</div>
 	);
 };
 
